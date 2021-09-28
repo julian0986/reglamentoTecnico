f0e669cee659277719a1ec3c7da60168b2c5e034
Diego <drbv27@gmail.com>
2021-09-28 11:41:25 -0500

    Implementacion cuenta regresiva

---
+++ src/js/app2.js
@@ -0,0 +1,54 @@
+const d = document;
+
+/* export default function countdown(id, limitDate, finalMessage) { */
+function countdown(id, limitDate, finalMessage, message) {
+  const $countdown = d.getElementById(id),
+    countdownDate = new Date(limitDate).getTime();
+
+  let countdownTempo = setInterval(() => {
+    let now = new Date().getTime(),
+      limitTime = countdownDate - now,
+      days = Math.floor(limitTime / (1000 * 60 * 60 * 24)),
+      hours = (
+        "0" + Math.floor((limitTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+      ).slice(-2),
+      minutes = (
+        "0" + Math.floor((limitTime % (1000 * 60 * 60)) / (1000 * 60))
+      ).slice(-2),
+      seconds = ("0" + Math.floor((limitTime % (1000 * 60)) / 1000)).slice(-2);
+
+    $countdown.innerHTML = `<h3>${message}: ${days}d ${hours}h ${minutes}m ${seconds}s</h3>`;
+
+    // console.log(countdownDate,now,limitTime);
+
+    if (limitTime < 0) {
+      clearInterval(countdownTempo);
+      $countdown.innerHTML = `<h3>${finalMessage}</h3>`;
+    }
+  }, 1000);
+}
+
+countdown(
+  "countdown",
+  "Mar 30, 2022 12:00:00",
+  "Entrada en Vigencia",
+  "Entrada en Vigencia"
+);
+countdown(
+  "countdown2",
+  "Mar 30, 2023 12:00:00",
+  "Un año de Vigencia",
+  "Etapa 1"
+);
+countdown(
+  "countdown3",
+  "Mar 30, 2024 12:00:00",
+  "Dos años Vigencia",
+  "Etapa 2"
+);
+countdown(
+  "countdown4",
+  "Mar 30, 2025 12:00:00",
+  "Tres años de Vigencia",
+  "Etapa 3"
+);
