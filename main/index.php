b6cca7c4b65b6843b7da46730c3fce38837c1b1a
Julian Calle <julian0986@gmail.com>
2021-11-10 13:03:45 -0500

    me mueve la barra por mil veses

--- main/index.php
+++ main/index.php
@@ -2781,19 +2781,15 @@ if(isset($_SESSION) && !empty($_SESSION['CODSICOM']))
     </div>
     </div>
     </div>
-    <div class="styky" id="percent2">
-      <h2 class="text-primary">LISTA DE CHEQUEO, RESOLUCIÓN 40198 DE 2021.</h2>
-    <div>
-        <p>0%  Procentaje de cumplimiento requisitos especificos</p>
-        <div id="myProgress2">
-        <div id="myBar2"></div>
-      </div>
-      </div>
-    </div>
     <div class="card generales" id="option2">
       <h5 class="card-header">REQUISITOS ESPECÍFICOS (Automotriz)</h5>
       <!-- Barra de Requisitos Especificos -->
-      
+      <div class="styky" id="percent2">
+        <p>0%</p>
+      </div>
+      <div id="myProgress2">
+        <div id="myBar2"></div>
+      </div>
       <div class="card-body">
         <div class="accordion" id="accordionExample">
           <!--Primer acordeon-->
