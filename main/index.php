79de0a143bce0d59a41958568b2df7f395a758d7
Julian Calle <julian0986@gmail.com>
2021-11-10 10:57:56 -0500

    Cambio de stiky en barra

--- main/index.php
+++ main/index.php
@@ -57,7 +57,7 @@ if(isset($_SESSION) && !empty($_SESSION['CODSICOM']))
 
 
     </div>
-    <div id="tituloreglamento">
+    <div class="tituloreglamento" id="tituloreglamento">
       <h2 class="text-primary">LISTA DE CHEQUEO, RESOLUCIÓN 40198 DE 2021.</h2>
       <div id="percent">
         <p>0% Procentaje de cumplimiento</p>
--- main/src/js/bloc.js
+++ main/src/js/bloc.js
@@ -1,1 +1,23 @@
 document.oncontextmenu = function(){return false}
+let barra= document.getElementById('percent');
+let barra2=document.getElementById('percent2')
+let altura= barra.offsetTop;
+let altura2=barra2.offsetTop;
+
+window.addEventListener('scroll', function(){
+    if (window.scrollY > altura){
+        barra.classList.add('tituloreglamento')
+    }else{
+        barra.classList.remove('tituloreglamento')
+    } 
+        
+    
+})
+
+   window.addEventListener('scroll', function(){
+    if(window.scrollY>altura2){
+        barra.classList.remove('tituloreglamento')
+        
+    }
+    
+})
