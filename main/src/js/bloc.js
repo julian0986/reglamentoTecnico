eb8adf1f2d502b57a3422c751ffa6083a2888a63
Julian Calle <julian0986@gmail.com>
2021-11-10 11:01:38 -0500

    Cambio de stiky en barra se agregara id

--- main/src/js/bloc.js
+++ main/src/js/bloc.js
@@ -1,5 +1,6 @@
 document.oncontextmenu = function(){return false}
-let barra= document.getElementById('percent');
+
+let barra= document.getElementById('tituloreglamento');
 let barra2=document.getElementById('percent2')
 let altura= barra.offsetTop;
 let altura2=barra2.offsetTop;
