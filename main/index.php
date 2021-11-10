fc73f70b93fd56f0f170667b3609d51ff3efdcbc
Julian Calle <julian0986@gmail.com>
2021-11-10 12:15:20 -0500

    prueba class styky al id percent

--- main/index.php
+++ main/index.php
@@ -57,9 +57,9 @@ if(isset($_SESSION) && !empty($_SESSION['CODSICOM']))
 
 
     </div>
-    <div class="styky" id="tituloreglamento">
+    <div id="tituloreglamento">
       <h2 class="text-primary">LISTA DE CHEQUEO, RESOLUCIÓN 40198 DE 2021.</h2>
-      <div id="percent">
+      <div class="styky" id="percent">
         <p>0% Procentaje de cumplimiento</p>
       </div>
       <div id="myProgress">
