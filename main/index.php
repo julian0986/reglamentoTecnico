88f693f245f69d5b7a0231c6eb4d16b53100e943
Julian Calle <julian0986@gmail.com>
2021-11-10 11:37:59 -0500

    prueba de styki clase par acrear stiky

--- main/index.php
+++ main/index.php
@@ -57,9 +57,9 @@ if(isset($_SESSION) && !empty($_SESSION['CODSICOM']))
 
 
     </div>
-    <div class="tituloreglamento" id="tituloreglamen">
+    <div id="tituloreglamen">
       <h2 class="text-primary">LISTA DE CHEQUEO, RESOLUCIÓN 40198 DE 2021.</h2>
-      <div id="percent">
+      <div class="tituloreglamento" id="percent">
         <p>0% Procentaje de cumplimiento</p>
       </div>
       <div id="myProgress">
